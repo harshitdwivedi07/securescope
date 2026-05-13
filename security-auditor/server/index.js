ba42df55cc8f211c3bb5f2e40c97e76e9c75ab9b
harshitdwivedi07 <dwivedih556@gmail.com>
2026-05-13 15:46:26 +0530

    update cors

--- security-auditor/server/index.js
+++ security-auditor/server/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://your-app.netlify.app"
+        "https://securescope.netlify.app"
     ]
 }));
 app.use(express.json());
